ad16bde116df0aa4c3ce8e2c4ac307bb1b887ccb
galifornia <martinho87@gmail.com>
2022-01-24 00:15:43 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div>
+      <h1>This is a new beginning</h1>
+    </div>
+  );
+};
+
+export default App;
