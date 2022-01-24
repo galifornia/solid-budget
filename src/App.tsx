aa4fea0b8c23e51912e4b6d06c7a39b2ae809792
galifornia <martinho87@gmail.com>
2022-01-24 17:21:19 +0100

    Add BudgetCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import { Button, Container, Stack } from 'solid-bootstrap';
 import type { Component } from 'solid-js';
+import BudgetCard from './components/BudgetCard';
 
 const App: Component = () => {
   return (
     <Container className='my-4'>
-      <Stack direction='horizontal' gap={2}>
+      <Stack className='d-flex' direction='horizontal' gap={2}>
         <h1 class='me-auto'>Budgets</h1>
         <Button variant='primary'>Add budget</Button>
         <Button variant='outline-primary'>Add Expense</Button>
       </Stack>
       <div
+        className='my-3'
         style={{
           display: 'grid',
           gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
           gap: '1rem',
           alignItems: 'flex-start',
         }}
-        // <BudgetCard />
-        // <BudgetCard />
-        // <BudgetCard />
-        // <BudgetCard />
-      ></div>
+      >
+        <BudgetCard name='Entertainment' gray amount={1200} max={1000} />
+      </div>
     </Container>
   );
 };
---
+++ src/components/BudgetCard.tsx
@@ -0,0 +1,57 @@
+import { Button, Card, ProgressBar, Stack } from 'solid-bootstrap';
+import { currencyFormatter } from '../utils';
+
+type Props = {
+  name: string;
+  amount: number;
+  max: number;
+  gray?: boolean;
+};
+
+const getProgressBarVariant = (amount: number, max: number) => {
+  const ratio = amount / max;
+  if (ratio < 0.5) return 'primary';
+  if (ratio < 0.75) return 'warning';
+  return 'danger';
+};
+
+const BudgetCard = (props: Props) => {
+  const classNames = [];
+  if (props.amount > props.max) {
+    classNames.push('bg-danger', 'bg-opacity-10');
+  } else if (props.gray) {
+    classNames.push('bg-light');
+  }
+  return (
+    <Card className={classNames.join(' ')}>
+      <Card.Body>
+        <Card.Title className='d-flex justify-content-between align-items-baseline fw-normal mb-3'>
+          <div className='me-2'>{props.name}</div>
+          <div className='d-flex align-items-baseline'>
+            {currencyFormatter.format(props.amount)}
+            <span className='text-muted fs-6 ms-1'>
+              / {currencyFormatter.format(props.max)}
+            </span>
+          </div>
+        </Card.Title>
+        <ProgressBar
+          className='rounded-pill'
+          variant={getProgressBarVariant(props.amount, props.max)}
+          min={0}
+          max={props.max}
+          now={props.amount}
+        />
+        <Stack
+          className='d-flex mt-4 justify-content-end'
+          direction='horizontal'
+          gap={2}
+        >
+          <Button variant='outline-primary'>Add expense</Button>
+          <Button variant='outline-secondary'>View Expense</Button>
+        </Stack>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default BudgetCard;
