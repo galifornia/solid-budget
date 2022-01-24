6018d2a07a794e4db240a7d7a8371699bc3c8caa
galifornia <martinho87@gmail.com>
2022-01-25 00:54:00 +0100

    Add localStorage Store for BudgetProvider state

--- context/BudgetProvider.tsx
+++ context/BudgetProvider.tsx
@@ -1,4 +1,10 @@
-import { createSignal, createContext, useContext } from 'solid-js';
+import {
+  createContext,
+  createEffect,
+  createSignal,
+  useContext,
+} from 'solid-js';
+import { createStore, SetStoreFunction, Store } from 'solid-js/store';
 import { v4 as uuidV4 } from 'uuid';
 
 const BudgetContext = createContext();
@@ -25,8 +31,17 @@ type State = {
   expenses: Expense[];
 };
 
+export function createLocalStore<T>(
+  initState: T
+): [Store<T>, SetStoreFunction<T>] {
+  const [state, setState] = createStore(initState);
+  if (localStorage.budgetState) setState(JSON.parse(localStorage.budgetState));
+  createEffect(() => (localStorage.budgetState = JSON.stringify(state)));
+  return [state, setState];
+}
+
 export const BudgetProvider = (props: Props) => {
-  const [state, setState] = createSignal<State>({
+  const [state, setState] = createLocalStore<State>({
       budgets: [],
       expenses: [],
     }),
@@ -34,38 +49,35 @@ export const BudgetProvider = (props: Props) => {
       state,
       {
         getBudgetExpenses(budgetId: string) {
-          return state().expenses.filter(
+          return state.expenses.filter(
             (expense) => expense.budgetId === budgetId
           );
         },
         addExpense(expense: Expense) {
-          const newExpenses = [
-            ...state().expenses,
-            { ...expense, id: uuidV4() },
-          ];
-          setState({ ...state(), expenses: newExpenses });
+          const newExpenses = [...state.expenses, { ...expense, id: uuidV4() }];
+          setState({ ...state, expenses: newExpenses });
         },
         addBudget(budget: Budget) {
           // Insert new budget only if name is not already taken
-          if (state().budgets.find((b) => b.name === budget.name)) {
-            return state();
+          if (state.budgets.find((b) => b.name === budget.name)) {
+            return state;
           }
 
-          const newBugdets = [...state().budgets, { ...budget, id: uuidV4() }];
-          setState({ ...state(), budgets: newBugdets });
+          const newBugdets = [...state.budgets, { ...budget, id: uuidV4() }];
+          setState({ ...state, budgets: newBugdets });
         },
         deleteBudget(id: string) {
           // !TODO: deal with uncategorized expenses
           setState({
-            ...state(),
-            budgets: state().budgets.filter((budget) => budget.id !== id),
+            ...state,
+            budgets: state.budgets.filter((budget) => budget.id !== id),
           });
         },
 
         deleteExpense(id: string) {
           setState({
-            ...state(),
-            expenses: state().expenses.filter((expense) => expense.id !== id),
+            ...state,
+            expenses: state.expenses.filter((expense) => expense.id !== id),
           });
         },
       },
