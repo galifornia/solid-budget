74fdfc90c514e21e1608fc625a16f3e14a833d5c
galifornia <martinho87@gmail.com>
2022-01-24 01:08:21 +0100

    Add uuid dep & prepare main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { Button, Container, Stack } from 'solid-bootstrap';
 import type { Component } from 'solid-js';
 
 const App: Component = () => {
   return (
-    <div>
-      <h1>This is a new beginning</h1>
-    </div>
+    <Container className='my-4'>
+      <Stack direction='horizontal' gap={2}>
+        <h1 class='me-auto'>Budgets</h1>
+        <Button variant='primary'>Add budget</Button>
+        <Button variant='outline-primary'>Add Expense</Button>
+      </Stack>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+          gap: '1rem',
+          alignItems: 'flex-start',
+        }}
+        // <BudgetCard />
+        // <BudgetCard />
+        // <BudgetCard />
+        // <BudgetCard />
+      ></div>
+    </Container>
   );
 };
 
